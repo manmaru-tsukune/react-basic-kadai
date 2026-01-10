19246f101d8e2d28a248fe8239dff83d36047c43
manmaru-tsukune <99634038+manmaru-tsukune@users.noreply.github.com>
2026-01-10 19:40:20 +0900

    react-basic-kadai
    アップロード

---
+++ react-basic-kadai/src/ProfileCard.jsx
@@ -0,0 +1,15 @@
+function ProfileCard({ name, age, bio }) {
+  return (
+    <div style={{
+      border: '1px solid #ccc',
+      borderRadius: '8px',
+      padding: '16px'
+    }}>
+      <h2>{name}</h2>
+      <p>【年齢】{age}歳</p>
+      <p>【自己紹介】{bio}</p>
+    </div>
+  )
+}
+
+export default ProfileCard
